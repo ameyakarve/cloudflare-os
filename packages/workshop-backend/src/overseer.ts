27901a2daa6dfb8d955070c7397cfda3bad13379
Vandana <vandanakarve@Vandanas-MacBook-Air.local>
2026-08-28 12:42:59 +0530

    Forward affiliate link mutation RPC methods

--- packages/workshop-backend/src/overseer.ts
+++ packages/workshop-backend/src/overseer.ts
@@ -8837,6 +8837,12 @@ export class GatekeeperLoopback extends WorkerEntrypoint<Cloudflare.Env, Gatekee
   getLink(...args: any[]): Promise<any> {
     return this.session.getLink(...args);
   }
+  createLink(...args: any[]): Promise<any> {
+    return this.session.createLink(...args);
+  }
+  updateLink(...args: any[]): Promise<any> {
+    return this.session.updateLink(...args);
+  }
   getLinkPerformance(...args: any[]): Promise<any> {
     return this.session.getLinkPerformance(...args);
   }
